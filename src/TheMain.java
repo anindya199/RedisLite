960166e3afebbfe3d321efff49bcaea7ba99659b
Anindya Sankar Bag <anindya.sankarbag99@gmail.com>
2026-09-03 00:02:01 +0530

    Implement TTL storage, SET EX, and EXPIRE commands

--- src/TheMain.java
+++ src/TheMain.java
@@ -23,17 +23,36 @@ public class TheMain {
             List<String> words = List.of(input.split("\\s+"));
 
             switch (words.get(0)) {
+
                 case "SET": {
-                    if(words.size() < 3)
+                    if (words.size() < 3)
                         System.out.println("Error: SET requires a Key and a Value");
                     else {
-                        Object val = words.get(2);
                         ValueType valType = ValueParser.detectType(words.get(2));
-                        Value result = new Value(valType,Persistence.getObject(valType, words.get(2)));
-                        db.SET(words.get(1),result);
+
+                        if (words.size() == 3) {
+                            Object val = words.get(2);
+                            Value result = new Value(valType, Persistence.getObject(valType, words.get(2)));
+                            db.SET(words.get(1), result);
+                        }
+                        else if (words.size() == 5 && words.get(3).equalsIgnoreCase("EX")) {
+                            try{
+                                long ttl = Long.parseLong(words.get(4));
+                                Value result = new Value(valType,Persistence.getObject(valType, words.get(2)),ttl);
+                                db.SET(words.get(1), result);
+                            }
+                            catch (NumberFormatException e){
+                                System.out.println("Error: The required type of EX to be a Number");
+                            }
+                        }
+                        else {
+                            System.out.println("Error: The syntax is Invalid");
+                        }
                     }
                     break;
                 }
+
+
                 case "GET": {
                     if(words.size()<2)
                         System.out.println("Error: GET requires a KEY");
@@ -44,6 +63,8 @@ public class TheMain {
                     }
                     break;
                 }
+
+
                 case "DELETE": {
                     if(words.size()<2)
                         System.out.println("Error: DELETE requires a KEY");
@@ -51,6 +72,8 @@ public class TheMain {
                         db.DELETE(words.get(1));
                     break;
                 }
+
+
                 case "EXISTS": {
                     if(words.size()<2)
                         System.out.println("Error: EXISTS requires a KEY");
@@ -58,18 +81,48 @@ public class TheMain {
                         System.out.println(db.EXISTS(words.get(1)));
                     break;
                 }
+
+
                 case "EXIT":{
                     flag = false;
                     break;
                 }
+
+
                 case "KEYS": {
                     System.out.println(db.KEYS());
                     break;
                 }
+
+
                 case "CLEAR": {
                     db.CLEAR();
                     break;
                 }
+
+
+                case "EXPIRE":{
+                    if(words.size() != 3)
+                        System.out.println("Error: EXPIRE requires a Key and a Value");
+                    else {
+                        try{
+                            long ttl = Long.parseLong(words.get(2));
+                            Value val = db.GET(words.get(1));
+                            if(val!=null){
+                                val.setExpireAt(ttl);
+                            }
+                            else{
+                                System.out.println("Error: Value not found");
+                            }
+                        }
+                        catch (NumberFormatException e){
+                            System.out.println("Error: The required type of EX to be a Number");
+                        }
+                    }
+                    break;
+                }
+
+
                 default:{
                     System.out.println("Invalid command");
                 }
@@ -83,6 +136,5 @@ public class TheMain {
         catch (IOException e) {
             throw  new IOException("Unable to create file");
         }
-
     }
 }
--- src/Value.java
+++ src/Value.java
@@ -1,15 +1,33 @@
+
 public class Value {
     private final ValueType type;
     private final Object value;
-
+    private long expireAt;
+//    private long ttl;
     public Value(ValueType type, Object value) {
         this.type = type;
         this.value = value;
+        expireAt = -1;
+    }
+    public Value(ValueType type, Object value,long ttl) {
+        this.type = type;
+        this.value = value;
+
+        long currentTime = System.currentTimeMillis();
+        expireAt = currentTime + ttl*1000;
     }
     public ValueType getType(){
         return type;
     }
     public Object getValue(){
         return value;
+    }
+    public boolean isExpired(){
+        return expireAt != -1 && expireAt <= System.currentTimeMillis();
+    }
+
+    public void setExpireAt(long ttl) {
+        long currentTime = System.currentTimeMillis();
+        expireAt = currentTime + ttl*1000;
     }
 }
